d857204f9b8d07c96d8d94595cfc7ba2ae28dc4c
Gustavo Arenas <gustavo.arenas73@gmail.com>
2016-11-15 13:46:40 +0000

    15 nov

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,19 +38,25 @@ $timeout( function(){ window.location = "#/app/home"; }, 3000);
 .controller('playlists', function ($scope, $http,$location) {
  
 })
-.controller('login', function ($state, $ionicLoading, $rootScope, $scope, $http, $location, SrvCall) {
+.controller('login', function ($state, $ionicLoading, $rootScope, $scope, $http, $location, SrvCall, $sessionStorage) {
+ 
+ if($sessionStorage.userLoged){
+      $state.go("app.home");
+ }
  
  $scope.login = function(){
       //inicia el evento cargando y bloquea la pantalla
     $ionicLoading.show({
         template: '<ion-spinner icon="android"></ion-spinner>'
     }); 
-    SrvCall.async('dummys/login.json', 'GET', '').success(function(resp) {
+    
+    //SrvCall.async('dummys/login.json', 'GET', '').success(function(resp) {
+    SrvCall.async('https://api.stormpath.com/v1/accounts/3bmLIdu6nbCEO7EzpxipZb', 'GET', '').success(function(resp) {
      //console.log(resp)
      //Apaga el evento cargando
      $ionicLoading.hide();
      $rootScope.userLoged = resp.user;
-     console.log(resp.user);
+     $sessionStorage.userLoged = $rootScope.userLoged;
      $state.go("app.home");
      
      }).error(function(resp){
@@ -66,8 +72,31 @@ $timeout( function(){ window.location = "#/app/home"; }, 3000);
  
 })
 
-.controller('register', function ($scope, $http,$location) {
+.controller('register', function ($state, $ionicLoading, $rootScope, $scope, $http, $location, SrvCall, $sessionStorage, $ionicPopup) {
  
+ $scope.register = function(){
+      //inicia el evento cargando y bloquea la pantalla
+    $ionicLoading.show({
+        template: '<ion-spinner icon="android"></ion-spinner>'
+    }); 
+    SrvCall.async('http://3BS3FAEHJ7A2PDFQPXDMNR8IC:S37xox/5wZxoBReqGuuBoVFXETJMXhcmgTT3tZ96ygA@api.stormpath.com/v1/applications/4EYncfTJgPkfPquDYKbBM/accounts', 'POST', '{\n    \"givenName\": \"Joe\",\n    \"surname\": \"Stormtrooper\",\n    \"username\": \"tk421\",\n    \"email\": \"tk421@stormpath.com\",\n    \"password\":\"Changeme1\",\n    \"customData\": {\n      \"favoriteColor\": \"white\"\n    }\n  }').success(function(resp) {
+     //console.log(resp)
+     //Apaga el evento cargando
+     $ionicLoading.hide();
+     $rootScope.userLoged = resp.userRegister;
+     $sessionStorage.userLoged = $rootScope.userLoged;
+     $state.go("app.home");
+     
+     }).error(function(resp){
+            //Apaga el evento cargando
+            $ionicLoading.hide();
+                $ionicPopup.alert({
+                        title: 'Ups!',
+                        template: resp,
+                        okText: 'OK!'
+                });     
+    });
+ } 
 })
 .controller('shipping', function ($scope, $http,$location) {
  
